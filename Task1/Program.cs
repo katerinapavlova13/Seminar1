509b9c2f1bd2a79edfb882f7d71f397621019172
Katerina Pavlova <malinakate18@gmail.com>
2022-08-05 12:41:07 +0400

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b * b == a)
+{
+    Console.WriteLine($"{a} является квадратом {b}");
+}
+else{
+    Console.WriteLine($"{a} не является квадратом {b}");
+}
